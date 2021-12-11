fa181f3142a49986c3a7b2fed444fbfcaa203093
Deslate <deslate@outlook.com>
2021-12-11 16:06:03 +0800

    Banner 滚动效果尝试

--- src/App.js
+++ src/App.js
@@ -6,22 +6,33 @@ import React from 'react';
 import Particles from "react-tsparticles";
 
 import './opening.css'
+import './overview.css'
 import { message, Space } from 'antd';
 import NetworkGraph from './components/network/network';
 
 export const white_paper_link = "https://www.deainet.io/file/Whitepaper.pdf"
+
+const dev_mode = true; // controls whether the opening screen should show.
 
 function App() {
 
     const [ clicked, setClicled ] = React.useState(false);
 
     const [ tab, setTab ] = React.useState("overview");
-    
-    const particles_container = React.useRef();
+
+    const [ scrollTop, setScrollTop ] = React.useState(0)
+
+    React.useEffect(()=>{
+        window.addEventListener('scroll', (event) => {
+            let scrollTop = document.scrollingElement.scrollTop;
+            setScrollTop(scrollTop)
+            console.log(scrollTop > (window.innerHeight - 250)? (window.innerHeight - scrollTop - 100) / 150 : 1)
+        });
+    },[])
 
     return (
         <div className="App">
-            <div className={clicked?"App-opening__clicked":"App-opening"}>
+            <div hidden={dev_mode} className={clicked?"App-opening__clicked":"App-opening"}>
                 <div className="App-opening-center">
                     <div class={`${clicked?"card__clicked":"card"}`} onClick={()=>setClicled(true)}>
                         <div className="card-content App-card">
@@ -46,8 +57,8 @@ function App() {
                     
                     <button
                         className={`App-menu-item${tab==="overview"?"__selected":""}`}
-                        onClick={()=>{alert("SUccess")}
-                    }>Overview</button>
+                        onClick={()=>{alert("SUccess")}}
+                    >Overview</button>
 
                     <button className="App-menu-item" onClick={()=>{window.open(white_paper_link)}}>White Paper</button>
                     <button className="App-menu-item" onClick={()=>{message.info("Comming Soon")}}>AI Miners</button>
@@ -56,14 +67,62 @@ function App() {
                 </div>
             </header>
 
-            <NetworkGraph/>
+            <div id="banner" style={{
+                zIndex: 1,
+                position: 'relative',
+                top: scrollTop * 0.6,
+                transition: 'all 0.5s linear',
+                opacity: (scrollTop > (window.innerHeight - 250))? (window.innerHeight - scrollTop - 100) / 150 : 1,
+            }}>
+                <NetworkGraph height={window.innerHeight}/>
+            </div>
 
             <div className="App-contact-card-positioner">
-                <div className="App-contact-card">
+                <div className="App-contact-card" style={{opacity:scrollTop>100?0:1}}>
                     <h2>Contact Us:</h2>
                     <ContactBar/>
                 </div>
             </div>
+
+            {/* <div className='sep'>
+                <div className='sep-dual'>
+                    <div className="sep-dual-left"/>
+                    <div className="sep-dual-right"/>
+                </div>
+            </div> */}
+            
+
+            <div className='Overview' style={{zIndex:3}} >
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+                <h1>Hhahahahaha</h1>
+            </div>
+
+            <footer>
+            <ContactBar/>
+            </footer>
            
         </div>
     );
--- src/components/network/network.js
+++ src/components/network/network.js
@@ -7,15 +7,15 @@ import './network.css'
 import { white_paper_link } from "../../App";
 
 
-const NetworkGraph = () => {
+const NetworkGraph = ({style, className, height}) => {
 
-
+    const _height = height ? height : window.innerHeight
     
     const deg2rad = deg => { return deg * Math.PI / 180; }
     const rad2deg = rad => { return rad * 180 / Math.PI; }
     
 
-    const distance = 1000;
+    const distance = 800;
 
     // const graphElem = document.getElementById("3d-graph");
 
@@ -24,7 +24,7 @@ const NetworkGraph = () => {
     const [ gData, setGData ] = React.useState({nodes:[],links:[]})
 
     React.useEffect(()=>{
-        const N = 300;
+        const N = 200;
         const nodes = [...Array(N).keys()].map(i => {
             return { 
                 id: i,
@@ -50,20 +50,14 @@ const NetworkGraph = () => {
         setGData({nodes, links});
     },[])
 
-
-    // Graph.linkWidth(5);
-
     React.useEffect(()=>{
         if(graph.current){
             const bloomPass = new UnrealBloomPass();
-            bloomPass.strength = 1.2;
+            bloomPass.strength = 3;
             bloomPass.radius = 0.3;
-            bloomPass.threshold = 0.5;
+            bloomPass.threshold = 0.3;
             graph.current.postProcessingComposer().addPass(bloomPass)
         }
-
-        
-
     },[graph])
 
     const [ angle, setAngle ] = React.useState(0)
@@ -84,18 +78,6 @@ const NetworkGraph = () => {
         },100)
     },[angle])
 
-    
-
-    // let currentAngle = 0;
-    // setInterval(() => {
-    //     // Graph.cameraPosition({
-    //     //     x: distance * Math.sin(deg2rad(currentAngle)),
-    //     //     z: distance * Math.cos(deg2rad(currentAngle))
-    //     // });
-        
-    //     currentAngle += 0.5;
-    // }, 10);
-
     // window.addEventListener('resize', e => {
     //     let width = window.innerWidth;
     //     let height = window.innerHeight;
@@ -107,13 +89,21 @@ const NetworkGraph = () => {
 
 
     return (
-        <div id="3d-graph" style={{zIndex:1}}>
-            <div style={{opacity:0.6}}>
+        <div
+            id="3d-graph"
+            style={{
+                zIndex:1,
+                height:_height,
+                ...style
+            }}
+            className={className}
+        >
+            <div style={{opacity:0.8}}>
                 <ForceGraph3D
-                    height={window.innerHeight}
+                    height={_height}
                     ref={graph}
                     graphData={gData}
-                    nodeRelSize={4}
+                    nodeRelSize={2}
                     nodeOpacity={1}
                     linkWidth={2}
                     nodeAutoColorBy={"#161922"}
