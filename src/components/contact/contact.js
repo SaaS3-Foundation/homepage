37691f2126d8862ff193b12f51791ea3c2a28fd2
qinwang-ai <wangq49@mail2.sysu.edu.cn>
2021-12-13 02:25:40 +0800

    add social links

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -8,6 +8,10 @@ import {
 
 import './contact.css'
 import { Space } from 'antd'
+import {white_paper_link} from "../../App";
+export const twitter_link = "https://twitter.com/DeAINetOfficial"
+export const telegram_link = "https://t.me/deainet"
+export const github_link = "https://github.com/DeAINet"
 
 const ContactBar = ({className}) => {
     return (
@@ -15,23 +19,23 @@ const ContactBar = ({className}) => {
 
             <div className="contact-unfold">
                 <MailOutlined className="contact-icon"/>
-                <div className="contact-unfold-text">contact@deainet.io</div>
+                <div className="contact-unfold-text" onClick={()=>{window.open("mailto:contact@deainet.io")}}>contact@deainet.io</div>
             </div>
 
             
             <div className="contact-unfold contact-unfold-short">
                 <TwitterOutlined className="contact-icon"/>
-                <div className="contact-unfold-text">Twitter @DeAINet</div>
+                <div className="contact-unfold-text" onClick={()=>{window.open(twitter_link)}}>@DeAINet</div>
             </div>
 
             <div className="contact-unfold contact-unfold-short">
                 <SendOutlined className="contact-icon" style={{fontSize:24,paddingTop:2}}/>
-                <div className="contact-unfold-text">Telegram：DeAI</div>
+                <div className="contact-unfold-text" onClick={()=>{window.open(telegram_link)}}>https://t.me/deainet</div>
             </div>
 
             <div className="contact-unfold">
                 <GithubOutlined className="contact-icon" style={{fontSize:24,paddingTop:2}}/>
-                <div className="contact-unfold-text">Grihub：DeAINet</div>
+                <div className="contact-unfold-text" onClick={()=>{window.open(github_link)}}>https://github.com/DeAINet</div>
             </div>
 
             
