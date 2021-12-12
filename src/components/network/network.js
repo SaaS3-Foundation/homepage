b39ccc951cac82820b48f9bcb9d6481381f4470c
Deslate <deslate@outlook.com>
2021-12-12 20:42:09 +0800

    加pichdeck

--- src/components/network/network.js
+++ src/components/network/network.js
@@ -105,6 +105,7 @@ const NetworkGraph = ({style, className, height}) => {
             <div style={{opacity:0.8}}>
                 <ForceGraph3D
                     height={_height}
+                    width={window.innerWidth}
                     ref={graph}
                     graphData={gData}
                     nodeRelSize={2}
