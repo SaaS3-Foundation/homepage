dfa1dc5584b7e7048041d74e74a7ed56cd1fefb8
qinwang-ai <wangq49@mail2.sysu.edu.cn>
2021-12-24 19:53:11 +0800

    scroll-optimization

--- src/App.js
+++ src/App.js
@@ -25,14 +25,14 @@ export const white_paper_link = "https://www.deainet.io/file/Whitepaper.pdf"
 export const pichdeck_link = "https://www.deainet.io/file/PitchDeck_DeAI.pdf"
 const interval_height = '12rem'
 
-const dev_mode = false;
 // const dev_mode = false;
+const dev_mode = true;
 // controls whether the opening screen should show.
 
 export const goToOverview = () => {
     let el = document.getElementById('overview');
-    if(el){
-        el.scrollIntoView({ behavior: 'smooth', block:'center'})
+    if(el) {
+        el.scrollIntoView({behavior: 'smooth', block: 'center'})
     }
 }
 
@@ -50,6 +50,13 @@ function App() {
             let scrollTop = document.scrollingElement.scrollTop;
             setScrollTop(scrollTop)
         });
+        const textarea = document.querySelector('body');
+        const log = document.getElementById('log');
+
+        textarea.onscroll = logScroll;
+        function logScroll(e) {
+            console.log(`Scroll position: ${e.target.scrollTop}`);
+        }
     },[])
 
     const [ welcome_hidden, setWelcomeHidden ] = React.useState(dev_mode)
@@ -65,7 +72,7 @@ function App() {
     },[clicked])
 
     return (
-        <div className="App">
+        <div className="App container y mandatory-scroll-snapping">
             <div hidden={welcome_hidden} className={clicked?"App-opening__clicked":"App-opening"}>
                 <div className="App-opening-center">
                     <div class={`${clicked?"card__clicked":"card"}`} onClick={()=>setClicled(true)}>
@@ -95,6 +102,7 @@ function App() {
                 setTab={setTab}
             />
 
+            <div>
             <div id="banner" style={{
                 zIndex: 1,
                 position: 'relative',
@@ -111,7 +119,7 @@ function App() {
                     <ContactBar/>
                 </div>
             </div>
-
+            </div>
             {/* <div className='sep'>
                 <div className='sep-dual'>
                     <div className="sep-dual-left"/>
@@ -146,8 +154,10 @@ function App() {
                         src={problemimg3}
                     />
                 </div>
-                <div style={{height:interval_height}} />
+                {/*<div style={{height:interval_height}} />*/}
+            </div>
 
+            <div className='Overview article'  style={{zIndex:3}} >
                 <h1>DeAI Passion & Goal</h1>
 
                 <p>
@@ -158,7 +168,9 @@ function App() {
                     Google AI</a> and further
                     deliver a fully decentralized SaaS solution for Web3.0.
                 </p>
-                <div style={{height:interval_height}} />
+                {/*<div style={{height:interval_height}} />*/}
+            </div>
+            <div className='Overview article'  style={{zIndex:3}} >
                 <h1>Who is DeAI User</h1>
                 <p>
                 DeAI users are the developers who require AI services for their products. In DeAI, we define decentralized API (DeAPI) to replace centralized API. All DeAPIs are deployed on DeAINet in a distributed way to provide decentralized AI services. AI user can send a decentralized request (DeRequest) to a DeAPI to get a decentralized response (DeResponse). The reliable miners in DeAINet execute all DeAPIs in 7x24 with DPoS consensus.
@@ -175,7 +187,9 @@ function App() {
                 </ul>
                 <p>
                 </p>
-                <div style={{height:interval_height}} />
+                {/*<div style={{height:interval_height}} />*/}
+            </div>
+            <div className='Overview article'  style={{zIndex:3}} >
                 <h1>DeAI Protocol</h1>
                 <p>
                   DeAINet is Constructed by DeAI Protocol with ERC-20 Token DEAI for Governance. The Protocol is Applied on Four Roles:
@@ -184,7 +198,9 @@ function App() {
                     className="article-img"
                     src={roles}
                 />
-                <div style={{height:interval_height}} />
+                {/*<div style={{height:interval_height}} />*/}
+            </div>
+            <div className='Overview article'  style={{zIndex:3}} >
                 <h1>Algorithm NFT & Marketplace</h1>
                 <p>
                 Once a DeAPI is accepted to deploy, a unique tradeable algorithm NFT is minted and sent to the inventor address as a digital patent. Whenever users pay ETH to access this DeAPI, 5% ETH is paid as patent fee to the algorithm NFT holder while another 95% to miners. So inventors earn patent fee by holding the NFT or selling it on the marketplace to earn DEAI.
@@ -202,42 +218,35 @@ function App() {
                 {/*            </p><p>*/}
                 {/*DEAI Stakers earn a proportion of $ETH from miners’ profit by staking DEAI to miners for DPoS (Delegation PoS) consensus*/}
                 {/*            </p>*/}
-                <div style={{height:interval_height}} />
+                {/*<div style={{height:interval_height}} />*/}
+            </div>
+            <div className='Overview article'  style={{zIndex:3}} >
                 <h1>Partners</h1>
                 <div>
                     <img style={{width:900,maxWidth:'calc(100vw - 20px)'}} src={partner}></img>
                 </div>
-                <div style={{height:interval_height}} />
+                {/*<div style={{height:interval_height}} />*/}
+            </div>
+
+            <div className='Overview article'  style={{zIndex:3}} >
                 <h1>Our Team</h1>
                 <div>
                     <img style={{width:900,maxWidth:'calc(100vw - 20px)'}} src={team}></img>
                 </div>
-            </div>
-            
 
-                {/*<div style={{height:20}}></div>*/}
 
-                {/*<p>You can smallen the image:</p>*/}
 
-                {/*<div className='col'>*/}
-                {/*    <img className="article-img" style={{width: 100}} src={image2}></img>*/}
-                {/*</div>*/}
-
-                {/*<div style={{height:20}}></div>*/}
-
-                {/*<p>You can center the image:</p>*/}
-
-                {/*<div className='col' style={{alignItems:'center'}}>*/}
-                {/*    <img className="article-img" style={{width: 100}} src={image2}></img>*/}
-                {/*</div>*/}
-            <footer>
+           {/*<footer>*/}
+                <div style={{marginTop:'5vh'}}>
                 <ContactBar/>
                 <a href="https://goo.gl/maps/L4vicaLurYULHQ8Z6" target={'_blank'}>
                     <img style={{width:'1rem', marginLeft:'1em'}} src={icon_location}/>
                     71 Nanyang Dr, NTU Innovation Center, Singapore 638075
                 </a>
-            </footer>
-           
+            </div>
+            {/*</footer>*/}
+            </div>
+
         </div>
     );
 }
