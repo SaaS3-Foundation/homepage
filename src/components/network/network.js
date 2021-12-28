7ae2d00fab9f3436a118e7c64096a029478f76ef
Deslate <deslate@outlook.com>
2021-12-28 14:21:30 +0800

    fix scroll css

--- src/components/network/network.js
+++ src/components/network/network.js
@@ -105,7 +105,7 @@ const NetworkGraph = ({style, className, height}) => {
             <div style={{opacity:0.8}}>
                 <ForceGraph3D
                     height={_height}
-                    width={document.scrollingElement.clientWidth}
+                    width={document.scrollingElement.clientWidth - 30}
                     ref={graph}
                     graphData={gData}
                     nodeRelSize={2}
