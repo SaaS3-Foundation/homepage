251aae8f4049232ff9d968a88df9428539f06ce5
xs818 <108477986+xs818@users.noreply.github.com>
2022-08-22 15:22:12 +0800

    Add contract information display

--- src/App.js
+++ src/App.js
@@ -1,302 +1,221 @@
-import logo from './logo.png';
-import './App.css';
-import './utils/usecase.css'
-import PipClock from './components/pip_clock/pip_clock';
-import ContactBar from './components/contact/contact';
+import logo from './logo.svg';
 import React from 'react';
+import './App.css';
+import { useState, useEffect } from 'react';
+import { ethers } from "ethers";
+import { DownloadOutlined, CreditCardOutlined, WalletOutlined, HomeOutlined, GithubOutlined } from '@ant-design/icons';
+import { Collapse, Button, Form, Input, Checkbox, Radio, Tabs, notification } from 'antd';
+import Highlight from 'react-highlight';
+import "highlight.js/styles/zenburn.css"
+import { codeBlock } from 'common-tags';
+const { TabPane } = Tabs;
+const { Panel } = Collapse;
 
-import './opening.css'
-import './overview.css'
-import { message, Steps } from 'antd';
-import NetworkGraph from './components/network/network';
-import {  } from '@ant-design/icons'
 
-import problemimg1 from './static/bg.png'
-import tri from './static/tri.png'
-import partner from './static/partner.png'
-import ss3 from './static/ss3.png'
-import lx from './static/lx.png'
-import dapi from './static/dAPI.png'
-import roles from './static/roles.png'
-import portait from './static/portait.png'
-import icon_location from './static/icon_location.png'
-import AppHeader from './components/app_header/AppHeader';
-import { isChrome } from './utils/platform';
+const onClickConnect = (setCurrentAccount, setProvider) => {
+  //client side code
+  if(!window.ethereum) {
+    notification.open({
+      message: 'Please install the MetaMask plugin.',
+      description:
+        'Website: https://metamask.io/',
+      onClick: () => {
+        console.log('Notification Clicked!');
+      },
+    });
+    window.open("https://metamask.io/","install metamsk","");
+    return
+  }
+  
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "rinkeby")
+  
+  // MetaMask requires requesting permission to connect users accounts
+  provider.send("eth_requestAccounts", [])
+  .then((accounts)=>{
+    if(accounts.length>0) setCurrentAccount(accounts[0])
+  })
+  .catch((e)=>console.log(e))
 
-export const white_paper_link = "/file/SaaS3Whitepaper.pdf"
-export const pichdeck_link = ""
-const interval_height = '12rem'
-
-// const dev_mode = false;
-const dev_mode = true;
-// controls whether the opening screen should show.
-
-export const goToOverview = () => {
-    let el = document.getElementById('overview');
-    if(el) {
-        el.scrollIntoView({behavior: 'smooth', block: 'center'})
-    }
+  setProvider(provider);
 }
 
-export const goToDemo = () => {
-    let el = document.getElementById('demo');
-    if(el) {
-        el.scrollIntoView({behavior: 'smooth', block: 'center'})
-    }
+const Write = () => {
+  const data = new FormData();
+  data.append('transaction', "www");
+  var headers = data.getHeaders();
+  const resp = fetch("", {
+    method: 'POST',
+    headers: headers,
+    // @ts-ignore
+    body: data,
+  });
 }
 
+
+
+const  WriteInput = (props) => {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      
+      {props.inputs.map((input, index) => (
+        <Form.Item
+        label={input.name}
+        name={input.name}
+        key= {index}
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input placeholder={input.type} />
+      </Form.Item>
+      ))}
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Write
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+const onChange = (e) => {
+  // console.log(e);
+};
+
+const onShow = (e, contract) => {
+  let index = e.pop();
+  console.log(index, contract)
+  // let name = abi[index].name
+  
+  // let s = contract.name
+  // console.log(s,"????")
+};
+
+const onRead = (item) => {
+  console.log(item, "???");
+}
+
+async function readCode(setCode, setAbi) {
+  const resp = await fetch("http://150.109.145.144:3000/saas3/dapi/detail?id=qz1BUkH3yK", {
+    method: 'GET',
+  
+    // @ts-ignore
+    // body: data,
+  })
+  if (!resp.ok) {
+    console.log(resp.status)
+    return
+  }
+  const res = await resp.json();
+  console.log(res, "???????")
+  setCode(res.data.requester)
+  setAbi(res.data.requesterAbi)
+
+}
 
 function App() {
 
-    const is_chrome = isChrome();
+  const [currentAccount, setCurrentAccount] = useState()
+  const [chainId, setChainId] = useState()
+  const [chainname, setChainName] = useState()
+  const [provider, setProvider] = useState()
+  const [code, setCode] = useState()
+  const [abi, setAbi] = useState([])
 
-    const [ clicked, setClicled ] = React.useState(dev_mode);
+  useEffect(() => {
+    if(!currentAccount || !ethers.utils.isAddress(currentAccount)) return
+    //client side code
+    if(!window.ethereum) return
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+   
+    provider.getNetwork().then((result)=>{
+      setChainId(result.chainId)
+      setChainName(result.name)
+    })
 
-    const [ tab, setTab ] = React.useState("overview");
+  },[currentAccount, chainId])
 
-    const [ scrollTop, setScrollTop ] = React.useState(0)
+  const contract = new ethers.Contract("0x83aE10CF14ECd2e02b11Fe12Ef1e1809da5f9D1d", abi);
 
-    React.useEffect(()=>{
-        window.addEventListener('scroll', (event) => {
-            let scrollTop = document.scrollingElement.scrollTop;
-            setScrollTop(scrollTop)
-        });
-        const textarea = document.querySelector('body');
-        const log = document.getElementById('log');
+  // const data = new FormData();
+  // data.append('transaction', "www");
+  // var headers = data.getHeaders();
+  useEffect(() => {
+    readCode(setCode, setAbi)
+  }, [])
+  
+  
+  return (
+    <div className="App">
+      
+      <Tabs defaultActiveKey="1" type="card" size="middle">
+        <TabPane tab="Code" key="1">
+        <Highlight className='solidity'>
+            {code}
+        </Highlight>
+        </TabPane>
+        <TabPane tab="Read Contract" key="2">
+          <Collapse onChange={onChange}>
+            {abi.map((item, index) =>{
+              return (item.stateMutability == "view" ? <Panel header={item.name} key={index} >
+                <p>{item.name}</p>
+              </Panel> : "")
+              
+            })}
+          </Collapse>
+        </TabPane>
+        <TabPane tab="Write Contract" key="3">
+          <Button type="primary" shape="round" icon={<WalletOutlined />} onClick={()=>onClickConnect(setCurrentAccount, setProvider)} size="large">
+                {currentAccount? 
+                  <span> 
+                    {currentAccount.substr(0, 5) + new Array(4).join('.') + currentAccount.substr(-3)}
+                  </span>
+                : "Connect Wallet"}
+          </Button>
+          <Collapse defaultActiveKey={['1']} onChange={onChange}>
+            {abi.map((item, index) =>{
+              return (item.stateMutability != "view" && item.type == "function" && item.inputs.length != 0 ? <Panel header={item.name} key={index}>
+                <WriteInput inputs = {item.inputs} />
+              </Panel> : "")
+            })}
+          </Collapse>
+        </TabPane>
+      </Tabs>
+      
 
-        textarea.onscroll = logScroll;
-        function logScroll(e) {
-            console.log(`Scroll position: ${e.target.scrollTop}`);
-        }
-    },[])
-
-    const [ welcome_hidden, setWelcomeHidden ] = React.useState(dev_mode)
-
-    React.useEffect(()=>{
-        if(clicked) {
-            setTimeout(()=>{
-                setWelcomeHidden(true)
-            },[1500])
-        }else{
-            setWelcomeHidden(false)
-        }
-    },[clicked])
-
-    return (
-        <div className={`App container y mandatory-scroll-snapping ${!is_chrome?'safari':''}`}>
-            <div hidden={welcome_hidden} className={clicked?"App-opening__clicked":"App-opening"}>
-                <div className="App-opening-center">
-                    <div className={`${clicked?"card__clicked":"card"}`} onClick={()=>setClicled(true)}>
-                        <div className="card-content App-card">
-                        <img
-                            src={logo}
-                            className={`App-logo${clicked?"__clicked":""}`}
-                        />
-                        <PipClock className="App-pip-clock" />
-                        </div>
-                    </div>
-
-                    <div hidden={clicked} className="App-link" href="https://mythrillfiction.com/" target="_blank">
-                        <h1 className="graph-title-shadow"style={{zIndex:-1,bottom:'auto'}}> oracle network in Web3 </h1>
-                        <h1 className="graph-title" style={{zIndex:2}}> oracle network in Web3 </h1>
-                    </div>
-                </div>
-
-                <div className="App-contact-positioner">
-                    <ContactBar className={`App-contact ${clicked?"contact_clicked":""}`}/>
-                </div>
-            </div>
-
-            <AppHeader
-                setClicled={setClicled}
-                tab={tab}
-                setTab={setTab}
-            />
-
-            <div>
-            <div id="banner" style={{
-                zIndex: 1,
-                position: 'relative',
-                top: scrollTop * 0.6,
-                transition: 'all 0.5s linear',
-                opacity: (scrollTop > (window.innerHeight - 500))? (window.innerHeight - scrollTop - 200) / 300 : 1,
-            }}>
-                <NetworkGraph height={window.innerHeight}/>
-            </div>
-
-            <div className="App-contact-card-positioner">
-                <div className="App-contact-card" style={{opacity:scrollTop>100?0:1}}>
-                    <h2>Contact Us:</h2>
-                    <ContactBar/>
-                </div>
-            </div>
-            </div>
-            {/* <div className='sep'>
-                <div className='sep-dual'>
-                    <div className="sep-dual-left"/>
-                    <div className="sep-dual-right"/>
-                </div>
-            </div> */}
-            
-
-            <div className='Overview article'  style={{zIndex:3}} >
-                
-                {/* id is used only for jummping, do not use on next <h1> if not needed */}
-                <div className='article_child'>
-                <h1 id='overview'>Blockchain Oracles are Unsafe</h1>
-                <div>
-                <p>
-                    If the source (such as sensors) directly submits the data to the tamper-proof distributed ledger without going through multiple intermediaries, the possibility of data manipulation will be greatly reduced. However, the Off-Chain computations are centralized on AWS-like servers.
-                </p></div>
-                    <img
-                        className="article-img"
-                        src={problemimg1}
-                        style={{width:'600px'}}
-                    />
-                </div>
-                {/*<div style={{height:interval_height}} />*/}
-            </div>
-
-            <div className='Overview article'  style={{zIndex:3}} >
-                <div className='article_child'>
-                <h1>Layer2 Trilemma</h1>
-                    <p>Decentralization, Scalability and Partition tolerance. SaaS3 approaches the trilemma with SaaS3 protocol and delivers a solution for the next generation Dapps. </p>
-                    <br></br>
-                     <img
-                        className="article-img"
-                        src={tri}
-                        style={{width:'500px'}}
-                    />
-                </div>
-            </div>
-            <div className='Overview article'  style={{zIndex:3}} >
-                <div className='article_child'>
-                <h1>Secure oracle as a Service</h1>
-                <p>
-                    Web2 API providers could deploy their serverless APIs on decentralized runtime (dRuntime). Each miner could run all the serverless Web2 APIs.
-                </p>
-                    <br></br>
-                     <img
-                        className="article-img"
-                        src={ss3}
-                        style={{width:'500px'}}
-                    />
-                </div>
-            </div>
-            <div className='Overview article'  style={{zIndex:3}} >
-                <div className='article_child'>
-                <h1>Trustful APIs</h1>
-                    <br></br>
-                             <img
-                        className="article-img"
-                        src={lx}
-                        style={{width:'900px'}}
-                    />
-                </div>
-            </div>
-             <div className='Overview article'  style={{zIndex:3}} >
-                <div className='article_child'>
-                <h1 id='demo' name='demo'>Usecase</h1>
-                    {/*<p style={{textAlign:'center'}}>dRuntime is the decentralized environment to run dAPIs on decentralized computation nodes (Miners).*/}
-                    {/*</p>*/}
-                    {/*<br/>*/}
-                    {/*         <img*/}
-                    {/*    className="article-img"*/}
-                    {/*    src={dapi}*/}
-                    {/*    style={{width:'500px'}}*/}
-                    {/*/><br/>*/}
-                 <p style={{textAlign:'center', marginTop:'5vh'}}>Blockchain oracles with trustful off-chain computation are the future of Web3 to involve rich functionalities.</p>
-                 <div className='usecase'>
-                     <div className='page-content'>
-                         <div className='card2'>
-                            <div className='content'>
-                                <h2 className='title'>Digital Avatar</h2>
-                                <p className='copy'>
-                                    Avatar is your alter ego living in the decentralized world. Let us spare you the rocket knowledge to build one. We introduce dAPIs to create a metaverse image for you to meet with friends or co-workers.
-                                </p>
-                                <button className='btn' onClick={()=>{window.open('https://www.saas3.io/demo_dp', '_blank')}}>Try</button>
-                            </div>
-                         </div>
-                          <div className='card2'>
-                            <div className='content'>
-                                <h2 className='title'>Game Bot</h2>
-                                <p className='copy'>
-                                    Thanks to reinforcement learning, our dAPIs equip developers with ever-brainier bots. Familiarize bots with game mechanisms and economic regimes to enrich the gaming experience. Build best GameFi experiences.
-                                </p>
-                                <button className='btn' onClick={()=>{window.open('https://www.saas3.io/demo_pve', '_blank')}}>Try</button>
-                            </div>
-                         </div>
-                          <div className='card2'>
-                            <div className='content'>
-                                <h2 className='title'>NFT Generation</h2>
-                                <p className='copy'>
-                                    Our dAPIs help you demystify engineering a fleet of NFTs.  Developers can grab a handy API to kick-start and fast-track their work. Create, customize, and assemble your digital collectibles.
-                                </p>
-                                <button className='btn' onClick={()=>{window.open('https://www.saas3.io/demo_ainft', '_blank')}}>Try</button>
-                            </div>
-                         </div>
-                          <div className='card2'>
-                            <div className='content'>
-                                <h2 className='title'>dQuant for DeFi</h2>
-                                <p className='copy'>
-                                    Navigate quantitative trading with ease. Harness the latest algorithms developed by AI whizzes. Enabled by deep learning, our dAPIs are your go-to cash cow.
-                                </p>
-                                <button className='btn' onClick={()=>{window.open('https://www.saas3.io/demo_defi', '_blank')}}>Try</button>
-                            </div>
-                         </div>
-                         <div className='card2'>
-                            <div className='content'>
-                                <h2 className='title'>NFT Crossverse</h2>
-                                <p className='copy'>
-                                    Crossverse: Topple down boundaries between metaverse realms. Give every NFT a ready-made character in all virtual worlds. Use dAPI to switch between your decentralized identities seamlessly.
-                                </p>
-                                {/*<button className='btn' onClick={()=>{window.open('https://www.saas3.io/demo_defi', '_blank')}}>Try</button>*/}
-                            </div>
-                         </div>
-                     </div>
-                 </div>
-                </div>
-            </div>
-            <div className='Overview article'  style={{zIndex:3}} >
-                <div className='article_child'>
-                <h1>Tokenomics</h1>
-                <p>
-                SAAS is the utility token for governance and value store.
-Web2 API creators earn royalty fees by deploy a serverless API on the dRuntime to serve Dapps.
-Dapps (Smart Contracts) pay royalty fees and Gas fees to access APIs.
-Miners (Computation nodes) earn Gas fees in executing the APIs.
-Token Stakers profit from APIs royalty fees by staking to APIs to back their quality security (DPoS).
-                </p>
-                <div>
-                <img
-                    className="article-img"
-                    src={roles}
-                    style={{width:'500px'}}
-                /></div>
-                {/*<div style={{height:interval_height}} />*/}
-                </div>
-            </div>
-
-            <div className='Overview article'  style={{zIndex:3}} >
-                <div className='article_child'>
-                <h1>Partners</h1>
-                    <img className={'article-image'} src={partner}></img>
-                {/*<div style={{height:interval_height}} />*/}
-
-           <footer>
-                <div style={{position:'absolute', bottom:'10vh', width:'calc(100% - 96px)'}}>
-                <ContactBar/>
-                <a href="https://goo.gl/maps/L4vicaLurYULHQ8Z6" target={'_blank'}>
-                    <img style={{width:'1rem', marginLeft:'1em'}} src={icon_location}/>
-                    71 Nanyang Dr, NTU Innovation Center, Singapore 638075
-                </a>
-                </div>
-            </footer>
-                </div>
-            </div>
-
-        </div>
-    );
+     
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Marketplace from './pages/marketplace';
-import ApiInfo from './pages/api_info';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-      <Route exact path='/' element={<App/>} />
-      <Route exact path='/marketplace' element={<Marketplace/>} >
-      </Route>
-      <Route exact path='/api_info/:id' element={<ApiInfo/>} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
