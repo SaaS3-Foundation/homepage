4999e979af361113566f5293b1e4aae29ea2a436
Steven Wong <wangqin4377@qq.com>
2022-08-26 11:33:06 +0800

    Marketplace improvement

--- src/pages/marketplace/marketplace.js
+++ src/pages/marketplace/marketplace.js
@@ -8,6 +8,7 @@ import { Carousel } from 'react-responsive-carousel';
 import axios from 'axios';
 import { useState } from 'react';
 import config from '../../config.json';
+import api_logo from '../../SaaS3Logo.png';
 
 // const dev_mode = false;
 const dev_mode = true;
@@ -51,18 +52,7 @@ function Marketplace() {
     })}, []);
 
     
-    let data_list = [];
-    if (data['data']['list'].length > 4){
-        let i =4;
-        while (i<data['data']['list'].length){
-            let tmp = data['data']['list'].slice(i-4, i);
-            i = i+4;
-            data_list.push(tmp);
-        }
-        data_list.push(data['data']['list'].slice(i-4, data['data']['list'].length));
-    } else {
-        data_list.push(data['data']['list']);
-    }
+    let data_list = data['data']['list'];
     
     
     return (
@@ -74,58 +64,32 @@ function Marketplace() {
                 setTab={setTab}
             />
 
-            {/* <Carousel autoPlay interval="5000" transitionTime="5000" infiniteLoop> */}
-            <div  className='api-card-positioner' style={{width:'80%',  marginLeft: '10%', padding:'3rem'}}>
-            <Carousel
-                showArrows={true} //是否展示左右箭头
-                showStatus={false} //是否展示右上角状态
-                showIndicators={true} // 是否展示指示器
-                infiniteLoop={true} //时候循环
-                autoPlay={false} //是否自动播放
-                interval="7000"
-                stopOnHover={true} //  鼠标放上去是否停止播放
-                showThumbs={false} // 是否展示轮播缩放展示图
-                useKeyboardArrows={false} // 是否使用键盘左右按钮滑动
-                swipeable={false} // 是否刷新
-                // dynamicHeight={false} //动态高度
-                emulateTouch={false} // 是否拖拽滑动
-                style = {{width: '100px'}}
-                // style={{height:'100vh'}}
-            >
-                 {data_list.length>0 && data_list.map((item, index) => {
-                    return <div key={index} style={{padding:'10%'}}>
-                    <div className='grid'>
-                    { item.map((card, index1) => {
-                        let target_url = '/api_info/'.concat(card.id);
-                        return <div>
-                        <div  key={index1} 
-                        className="full border api-card" 
-                        >
-                           <h1>{card.title}</h1>
-                           <h2>{card.description}</h2>
-                           <h3> Created By {card.creator}</h3>
-                           <div style={{display:'flex'}}>
-                            {card.tags?.length>0 && card.tags?.map((tag, tag_index)=>{
-                            return <div style={{color:'white', fontSize:'1px',padding:'1px'}}>{tag}</div>
-                           })
-                           }</div>
-                           <button onClick={()=>{window.location.href=target_url}}
-                           >
-                            Learn More</button>
-                        </div>
-                        </div>
-                    }
-                    )}
-                    </div>
-                    <p className="legend"></p>
-                    </div>
-                    })
-                }
-            </Carousel>
+            <div className='api-card-positioner' style={{width:'80%',  marginLeft: '10%', padding:'3rem'}}>
+            <div style={{
+                        "display": "grid",
+                        "gridTemplateColumns": "1fr 1fr 1fr 1fr",
+                        "height":"max-content",
+                        "marginTop":"20px"}}>
+                            {data_list.length>0 && data_list.map((card, index) => {
+                            let target_url = '/api_info/'.concat(card.id);
+                            return <div key={index} className="api-card" onClick={()=>{window.location.href=target_url}}>
+                                        <div style={{"display":"flex", "flexDirection":"column","justifyContent":"space-between"}}>
+                                        <h1>{card.title}</h1>
+                                        <div style={{"text-align":"center"}}>
+                                        <img src={api_logo} style={{"width":"90%"}}/>
+                                        </div>
+                                        <h2>{card.description}</h2>
+                                        <h3> Created By {card.creator}</h3>
+                                            <div style={{display:'flex'}}>
+                                                {card.tags?.length>0 && card.tags?.map((tag, tag_index)=>{
+                                                return <div style={{color:'white', fontSize:'1px',padding:'1px'}}>{tag}</div>
+                                            })}
+                                            </div>
+                                        </div>
+                                    </div>
+                            })}
             </div>
-            
-
-
+            </div>
         </div>
     );
 }
