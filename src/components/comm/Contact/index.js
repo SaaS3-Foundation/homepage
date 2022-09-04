96811c2dcca8c18ff3bc02340457eca442f9a90d
Steven Wong <wangqin4377@qq.com>
2022-09-04 16:10:34 +0800

    Update index.js

--- src/components/comm/Contact/index.js
+++ src/components/comm/Contact/index.js
@@ -9,9 +9,10 @@ import './contact.scss';
 import { Space } from 'antd';
 export const email_link = 'mailto:contact@saas3.io';
 export const twitter_link = 'https://twitter.com/SaaS3Lab';
-export const telegram_link = 'https://t.me/layerXnetwork';
+export const telegram_link = 'https://t.me/saas3dao';
 export const github_link = 'https://github.com/SaaS3-Foundation';
 export const map_link = 'https://goo.gl/maps/L4vicaLurYULHQ8Z6';
+export const discord_link = 'https://discord.gg/dNENvenx';
 
 
 
@@ -20,7 +21,7 @@ const ContactBar = ({ className }) => {
   const contacts = [
     { title: 'contact@saas3.io', href: email_link, icon: <MailOutlined /> },
     { title: '@SaaS3Lab', href: twitter_link, icon: <TwitterOutlined /> },
-    { title: '@SaaS3Lab', href: telegram_link, icon: <SendOutlined /> },
+    { title: '@SaaS3DAO', href: telegram_link, icon: <SendOutlined /> },
     { title: 'SaaS3-Foundation', href: github_link, icon: <GithubOutlined /> },
     { title: 'Singapore', href: map_link, icon: <HomeOutlined /> },
   ];
